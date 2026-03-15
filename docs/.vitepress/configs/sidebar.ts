0391e290903d0005c7b5dccc6894f9bc9fc729a1
clb <709703075@qq.com>
2026-03-15 17:51:52 +0800

    chore: 删除文件进行测试

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -7,19 +7,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       text: 'AI日报',
       collapsed: false, 
       items: [
-        { text: '2026-03-14', link: '/AI/daily/cn/2026-03/2026-03-14' },
-        { text: '2026-03-13', link: '/AI/daily/cn/2026-03/2026-03-13' },
-        { text: '2026-03-12', link: '/AI/daily/cn/2026-03/2026-03-12' },
-        { text: '2026-03-11', link: '/AI/daily/cn/2026-03/2026-03-11' },
-        { text: '2026-03-10', link: '/AI/daily/cn/2026-03/2026-03-10' },
-        { text: '2026-03-09', link: '/AI/daily/cn/2026-03/2026-03-09' },
-        { text: '2026-03-08', link: '/AI/daily/cn/2026-03/2026-03-08' },
-        { text: '2026-03-07', link: '/AI/daily/cn/2026-03/2026-03-07' },
-        { text: '2026-03-06', link: '/AI/daily/cn/2026-03/2026-03-06' },
-        { text: '2026-03-05', link: '/AI/daily/cn/2026-03/2026-03-05' },
-        { text: '2026-03-04', link: '/AI/daily/cn/2026-03/2026-03-04' },
-        { text: '2026-03-03', link: '/AI/daily/cn/2026-03/2026-03-03' },
-        { text: '2026-03-02', link: '/AI/daily/cn/2026-03/2026-03-02' },
+        
         { text: '2026-03-01', link: '/AI/daily/cn/2026-03/2026-03-01' }
       ]
     },
@@ -34,8 +22,6 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       text: 'AI周报',
       collapsed: false,
       items: [
-        { text: '2026-03-01周报', link: '/AI/weekly/cn/2026-03-01-hexi-2077-ai-signal-weekly-2026-w09-trillion-dollar-arms-race' },
-        { text: '2026-03-08周报', link: '/AI/weekly/cn/2026-03-08-ai-weekly-2026-w10-gpt-5-4-militarization-claude-code' },
         { text: '2026-03-15周报', link: '/AI/weekly/cn/2026-03-15-ai-signal-weekly-2026-w11-anthropic-agent-ecosystem-war' }
       ]
     }
